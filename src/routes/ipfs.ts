ddab43de1c230466850228a394c88df24b500835
ququzone <xueping.yang@gmail.com>
2021-09-13 16:28:03 +0800

    add welcome message

--- src/routes/ipfs.ts
+++ src/routes/ipfs.ts
@@ -6,6 +6,9 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    res.status(200).send('Hello, NFT!')
+});
 router.post('/ipfs/upload', upload.single('file'), controller.upload);
 
 export = router;
