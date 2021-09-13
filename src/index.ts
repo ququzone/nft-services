a635b629eb4ae0f7e2576880f7557a6c665cafa9
ququzone <xueping.yang@gmail.com>
2021-09-13 17:40:20 +0800

    change to vercel

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,35 @@
-import http from 'http';
 import express, { Express } from 'express';
+import multer from 'multer';
 import morgan from 'morgan';
-import routes from './routes/ipfs';
+import ipfs from './controllers/ipfs'
 
-const router: Express = express();
+const app: Express = express();
+const upload = multer({ storage: multer.memoryStorage() });
 
-router.use(morgan('dev'));
-router.use(express.urlencoded({ extended: false }));
-router.use(express.json());
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST');
-    return res.status(200).json({});
-  }
-  next();
+app.use((req, res, next) => {  
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST');
+        return res.status(200).json({});
+    }
+    next();
 });
 
-router.use('/', routes);
+app.get('/', async (req, res) => {
+    res.status(200).send('Hello, NFT!');
+});
+app.get('/ipfs/upload', upload.single('file'), ipfs.upload);
 
-router.use((req, res, next) => {
-  const error = new Error('not found');
-  return res.status(404).json({
-    message: error.message,
-  });
+app.use((req, res, next) => {
+    const error = new Error('not found');
+    return res.status(404).json({
+        message: error.message,
+    });
 });
 
-const httpServer = http.createServer(router);
-const PORT: any = process.env.PORT ?? 3000;
-httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+export default app;
